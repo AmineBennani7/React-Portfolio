50e1ec8c7489f3977cbce67a699fb8c37a92de22
Amine Bennani <bennanou2000@gmail.com>
2024-08-29 18:21:54 +0200

    about me

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        Amine Bennani
       </a>
       <div className={styles.menu}>
         <img
