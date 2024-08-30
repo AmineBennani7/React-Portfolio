56b53281f0683b8558955392b7849f2652f8691d
Amine Bennani <bennanou2000@gmail.com>
2024-08-30 02:18:59 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/React-Portfolio/', // Asegúrate de reemplazar esto con el nombre de tu repositorio
 
   css: {
     modules: {
